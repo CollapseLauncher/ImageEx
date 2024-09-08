f457d126ce870ea57ab81bb0dc93558a9d22fa62
Bagus Nur Listiyono <dzakibagus@gmail.com>
2024-09-08 11:06:57 +0700

    Adjust project properties

--- ImageEx/Properties/AssemblyInfo.cs
+++ ImageEx/Properties/AssemblyInfo.cs
@@ -1,11 +1,3 @@
-﻿using System.Reflection;
-using System.Runtime.InteropServices;
+﻿using System.Runtime.InteropServices;
 
-[assembly: AssemblyTitle("ImageEx")]
-[assembly: AssemblyDescription("Extended Image Control for UWP and WinUI apps")]
-[assembly: AssemblyProduct("ImageEx")]
-[assembly: AssemblyCopyright("FourSoft ©  2023")]
-
-[assembly: AssemblyVersion("2.1.0.0")]
-[assembly: AssemblyFileVersion("2.1.0.0")]
 [assembly: ComVisible(false)]
